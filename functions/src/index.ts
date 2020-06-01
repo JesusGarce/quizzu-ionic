0b5176f9e6f142652ff1ad4221086b48e94d7ca6
jesusgarce <tarraga.jesus@gmail.com>
2020-06-01 20:18:52 +0200

    Functions error #61

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
-import {Server} from '../../src/app/shared/models/server.model';
+// import {Server} from '../../src/app/shared/models/server.model';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -8,16 +8,17 @@ import {Server} from '../../src/app/shared/models/server.model';
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-const cors = require("cors")({ origin: true });
+const cors = require('cors')({origin: true});
 
 exports.hiWorld = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
         response.setHeader('Access-Control-Allow-Origin', '*');
-        response.send({text:"Hello from Firebase!"});
+        response.send({text: 'Hello from Firebase!'});
     });
 });
 
-exports.createServer = functions.pubsub.schedule("every 2 minutes")
+/*
+exports.createServer = functions.pubsub.schedule('every 2 minutes')
     .onRun(async () => {
         const server = new Server(new Date(), new Date().getDay(), 0);
         const serverResp = await admin.firestore().collection('server').add(JSON.parse(JSON.stringify(server)));
@@ -25,3 +26,4 @@ exports.createServer = functions.pubsub.schedule("every 2 minutes")
         await Promise.all(serverResp.docs.map((doc: any) => doc.ref.update({overdue: true})));
     });
 
+ */
